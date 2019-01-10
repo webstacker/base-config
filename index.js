7af72a5f59a1104366c0d5d372fb73d82ade952b
Stephen S <webstacker@outlook.com>
2019-01-10 17:10:02 +0000

    Add config files

---
+++ index.js
@@ -0,0 +1,32 @@
+const createModuleZero = require('module-zero');
+
+const m0 = createModuleZero({
+    files: '**/*',
+    devDependencies: {
+        "eslint": "^5.8.0",
+        "eslint-config-airbnb-base": "^13.1.0",
+        "eslint-config-prettier": "^3.1.0",
+        "eslint-plugin-import": "^2.14.0",
+        "eslint-plugin-prettier": "^3.0.0",
+        "fs-extra": "^7.0.0",
+        "jest": "^23.6.0",
+        "lint-staged": "^8.0.4",
+        "prettier": "^1.14.3"
+    },
+    blocks: {
+        src: '**/*',
+        commentStyles: {
+            '#': '#! m0',
+            '//': '//! m0',
+            '/**/': '/*! m0 */'
+        },
+        commentStyleMap: {
+            '.gitignore': '#',
+            '.js': '/**/'
+        }
+    }
+});
+
+m0.copyFiles();
+m0.createBlocks();
+m0.installDevDependencies();
